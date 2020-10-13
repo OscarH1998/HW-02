896f7f1bc70bebd6bea28e75ec85206530b1b727
Oscar <oscarrhernandezoh@gmail.com>
2020-10-12 20:13:29 -0500

    1st editor button
    brackeys color example and 1st editor button to duplicate cube

---
+++ Assets/Cube.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    void Start()
+    {
+        GenerateColor();
+    }
+
+    public void GenerateColor()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = Random.ColorHSV();
+    }
+
+    public void Reset()
+    {
+        GetComponent<Renderer>().sharedMaterial.color = Color.white;
+    }
+
+    public void SpawnCube()
+    {
+        GameObject newObject = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+    }
+}
